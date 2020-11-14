2cf44a90a7d7de25c33e827073ae584cc644514f
MasaharuDobashi <dobashi.masaharu@gmail.com>
2020-11-14 17:23:01 +0900

    Initial Commit

---
+++ TestAPI_SwiftUI/TestAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPI_SwiftUIApp.swift
+//  TestAPI_SwiftUI
+//
+//  Created by 土橋正晴 on 2020/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TestAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAPI_SwiftUITests/TestAPI_SwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  TestAPI_SwiftUITests.swift
+//  TestAPI_SwiftUITests
+//
+//  Created by 土橋正晴 on 2020/11/14.
+//
+
+import XCTest
+@testable import TestAPI_SwiftUI
+
+class TestAPI_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
